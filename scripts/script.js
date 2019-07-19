3db6b3978d6c8af439f4f7ee763d3cf7c8732b48
LatashaWanjira <latasha.wanjira@gmail.com>
2019-07-19 19:51:57 +0300

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+// window.alert("Hello World!")
+
+// document.write("Hello World")
+
+// console.log("Hello World");
+
+// console.log(5 + 6);
+
+
+var x = 5 + 5;
+
+console.log("John" + " " + 3)
+
+for (i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+
+var x = 5;
+var y = "5";
+
+if (x === y) {
+  console.log(true);
+} else {
+  console.log(false);
+}
